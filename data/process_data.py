e17789913af551e6fa5a7896008e7591098b9587
Dpardey <71354269+Dpardey@users.noreply.github.com>
2021-02-17 18:03:05 -0500

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -84,7 +84,7 @@ def save_data(df, database_filename):
     '''
     
     engine = create_engine('sqlite:///DisasterResponse.db')
-    df.to_sql('DisasterResponse', engine, index=False) 
+    df.to_sql('DisasterResponse', engine, index=False, if_exists = 'replace') 
      
 
 
